14c671ee0224cb6286d7734859d211ed86dce272
hejunxue <junxue_he@163.com>
2016-11-10 16:35:57 +0800

    init

---
+++ Assets/CallCamra.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallCamra : MonoBehaviour
+{
+
+    private WebCamTexture _camTexture;
+    public GameObject Bg;
+    void Start()
+    {
+        StartCoroutine(OpenDevice());
+    }
+
+
+    IEnumerator OpenDevice()
+    {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            if (devices.Length >= 1)
+            {
+                string deviceName = devices[0].name;
+
+                _camTexture = new WebCamTexture(deviceName, 300, 400, 15);
+                _camTexture.Play();
+
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (_camTexture && _camTexture.isPlaying)
+        {
+            Bg.GetComponent<Renderer>().material.mainTexture = _camTexture;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (_camTexture && _camTexture.isPlaying)
+        {
+            GUI.DrawTexture(new Rect(0, 0, 100, 80), _camTexture, ScaleMode.ScaleToFit);
+        }
+        if (GUILayout.Button("play"))
+        {
+
+            if (_camTexture)
+                _camTexture.Play();
+        }
+        if (GUILayout.Button("pause"))
+        {
+
+            if (_camTexture)
+                _camTexture.Pause();
+        }
+        if (GUILayout.Button("stop"))
+        {
+            if (_camTexture)
+                _camTexture.Stop();
+        }
+    }
+}
---
+++ Assets/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour
+{
+
+
+    //方向灵敏度  
+    public float RotateSensitivityX = 5F;
+    public float RotateSensitivityY = 5F;
+    public float MoveSensitivityX = 0.1F;
+    public float MoveSensitivityY = 0.1F;
+    public float sensitivityZoom = 0.01F;
+    private Vector3 targetPos = Vector3.zero + new Vector3(0, 0.8f, 0);
+    public Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))//totate
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, Input.GetAxis("Mouse X") * RotateSensitivityX);
+            transform.RotateAround(targetPos, target.right, Input.GetAxis("Mouse Y") * RotateSensitivityY);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)//zoom
+        {
+            float delta_z = Input.GetAxis("Mouse ScrollWheel");
+            transform.position = transform.position - (-delta_z * sensitivityZoom * (targetPos - transform.position));
+        }
+
+        if(Input.GetMouseButton(0))//move
+        {
+            transform.Translate(new Vector3(-Input.GetAxis("Mouse X") * MoveSensitivityX, -Input.GetAxis("Mouse Y") * MoveSensitivityY, 0));
+        }
+    }
+}
